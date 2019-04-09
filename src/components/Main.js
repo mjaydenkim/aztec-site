0d5046ed166439da8dac350568bbee2f06dd7c55
mjaydenkim <mjaydenkim@gmail.com>
2019-04-08 20:53:51 -0700

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import pic04 from '../images/pic04.jpg'
 
 class Main extends React.Component {
   render() {

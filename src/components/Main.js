aa0ae1fe4d1eaff9929f95a8c37f8a8a8f74a368
mjaydenkim <mjaydenkim@gmail.com>
2019-04-08 20:50:52 -0700

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -37,30 +37,8 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
-          <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
-          </ul>
+          <span className="image main"><img src={pic04} alt="" /></span>
+          <p>The slaves were generally people who had committed serious crimes such as theft, conquered people, or people who were forced to sell themselves into slavery to gain back debt (generally because of gambling). Slaves could buy themselves free if they had the means, or could gain freedom in different ways. Slaves were required to farm as well as work as general laborers or domestic servants among other jobs. Slaves generally weren't re-sold and were protected by law from abuse by their masters or anyone else. Talented slaves could go higher up on the social ladder and become estate owners. Slavery was not in-born and just because your parent was one doesn't mean you have to be one, and slaves had much better rights. If slaves married their masters, could show their master mistreated them, or had children with them, they could be set free.</p>
           {close}
         </article>
 
